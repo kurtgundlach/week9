1849b07d4721b59c893565bcca9a286aa7f5292d
kurtgundlach <kurtgundlach@seattleacademy.org>
2016-02-09 14:56:50 -0800

    add 02_kurt.js

--- 02_kurt.js
+++ 02_kurt.js
@@ -2,3 +2,15 @@ console.log(process.argv);
 var pin = process.argv[2];
 var status = process.argv[3];
 console.log("pin number " + pin + " turn " + status);
+var wpi = require('wiring-pi');
+console.log("wpi",wpi);
+wpi.setup("gpio");
+wpi.pinMode(pin*1,wpi.OUTPUT);
+var value = 1;
+function toggleBulb(){
+	wpi.digitalWrite(pin,value);
+	console.log("pin","value",value);
+	value = +!value;
+
+}
+setInterval(toggleBulb,500)
